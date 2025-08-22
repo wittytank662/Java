8b6fa062dedf547c59922d41e459b161c2f7097f
Sam <78057250+wittytank662@users.noreply.github.com>
2025-08-22 03:34:39 -0700

    added school stuff

---
+++ FusionJavaProgramming/1.05 and 1.06 Style and OO/orderOfOperations.java
@@ -0,0 +1,22 @@
+public class orderOfOperations {
+    public static void main(String[] args) {
+        /*
+            Order of Operations
+                *Programming follows the typical PEMDAS you may have learned in math class
+
+                New Operators:
+                    1. unary plus and unary minus
+                        symbols: +   -
+                        priority: 1
+                    2. Modulus/Remainder
+                        symbol: %
+                        priority: 2 (same as multiplication/division)
+                    3. Concatenation
+                        symbol: +
+                        priority: 3 (same as addition/subtraction)
+                    4. Assignment
+                        symbol: =
+                        priority: 4
+        */
+    }
+}
---
+++ FusionJavaProgramming/printStatements.java
@@ -0,0 +1,33 @@
+public class printStatements{
+    public static void main(String[] args){
+        //Single Comment
+
+        /*
+            Multiple
+            Line
+            Comment
+        */
+
+        //---------Print Statement-------------
+
+        /*
+            Print Statements:
+
+                Utilize the object oriented syntax to access the print functions
+                    *System.out._______
+
+                    1. print();     - Prints as it is and leaves the input string open
+                    2. printf();    - formatted print
+                    3. println();   - prints the string and then executes a carriage return
+
+        */
+
+        System.out.print("Hello World\n");
+        System.out.print("Hello\\n\t");
+        System.out.print("World\\n\n");
+        System.out.println();
+
+        System.out.println("Hello"); System.out.println("World");
+
+    }
+}

dde1acaf5d93f787892e45ad6a67a2811bd82f0b
Sam <78057250+wittytank662@users.noreply.github.com>
2025-09-08 02:13:38 -0700

    idk sumthing lol

---
+++ FusionJavaProgramming/1.12 Arithmatic + Pitfalls and Shortcuts/GradesV2.java
@@ -0,0 +1,74 @@
+public class GradesV2 {
+    public static void main(String[] args) {
+        
+        // Local Variables
+        int numTests = 0;       // Counts number of tests
+        int testGrade = 0;      // Individual test grade
+        int totalPoints = 0;    // Total points for all tests
+        double average = 0.0;   // Average grade
+
+
+        // Test 1
+        testGrade = 95;
+        numTests++;
+        totalPoints += testGrade;
+        average = totalPoints / (double)numTests;
+        System.out.println("Test #" + numTests + "    Test Grade: " + testGrade + "    Total Points: " + totalPoints + "     Average Score: " + average);
+        
+        // Test 2
+        testGrade = 73;
+        numTests++;
+        totalPoints += testGrade;
+        average = totalPoints / (double)numTests;
+        System.out.println("Test #" + numTests + "    Test Grade: " + testGrade + "    Total Points: " + totalPoints + "    Average Score: " + average);
+
+        // Test 3
+        testGrade = 91;
+        numTests++;
+        totalPoints += testGrade;
+        average = totalPoints / (double)numTests;
+        System.out.println("Test #" + numTests + "    Test Grade: " + testGrade + "    Total Points: " + totalPoints + "    Average Score: " + average);
+
+        // Test 4
+        testGrade = 82;
+        numTests++;
+        totalPoints += testGrade;
+        average = totalPoints / (double)numTests;
+        System.out.println("Test #" + numTests + "    Test Grade: " + testGrade + "    Total Points: " + totalPoints + "    Average Score: " + average);
+
+        // Test 5
+        testGrade = 67;
+        numTests++;
+        totalPoints += testGrade;
+        average = totalPoints / (double)numTests;
+        System.out.println("Test #" + numTests + "    Test Grade: " + testGrade + "    Total Points: " + totalPoints + "    Average Score: " + average);
+
+        // Test 6
+        testGrade = 41;
+        numTests++;
+        totalPoints += testGrade;
+        average = totalPoints / (double)numTests;
+        System.out.println("Test #" + numTests + "    Test Grade: " + testGrade + "    Total Points: " + totalPoints + "    Average Score: " + average);
+
+        // Test 7
+        testGrade = 80;
+        numTests++;
+        totalPoints += testGrade;
+        average = totalPoints / (double)numTests;
+        System.out.println("Test #" + numTests + "    Test Grade: " + testGrade + "    Total Points: " + totalPoints + "    Average Score: " + average);
+
+        // Test 8
+        testGrade = 68;
+        numTests++;
+        totalPoints += testGrade;
+        average = totalPoints / (double)numTests;
+        System.out.println("Test #" + numTests + "    Test Grade: " + testGrade + "    Total Points: " + totalPoints + "    Average Score: " + average);
+
+        // Test 9
+        testGrade = 100;
+        numTests++;
+        totalPoints += testGrade;
+        average = totalPoints / (double)numTests;
+        System.out.println("Test #" + numTests + "    Test Grade: " + testGrade + "   Total Points: " + totalPoints + "    Average Score: " + average);
+    }
+}
---
+++ FusionJavaProgramming/1.13 Challenge Program/CurrencyV1.java
@@ -0,0 +1,79 @@
+/**
+ * The Currency class converts an amount of money from a specific
+ * country into the equivalent currency of another country given 
+ * the current exchange rate.
+ *
+ * @author Charles Babbage (replace with your name)
+ * @version 06/02/17       (replace with today's date)
+ */
+public class CurrencyV1
+{
+    public static void main(String [ ] args)
+    {
+
+		// Message to user stating purpose
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        System.out.println("This program converts an amount of money");
+        System.out.println("from a specific country into the equivalent");
+        System.out.println("currency of another country given the current");
+        System.out.println("exchange rate.");
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        System.out.println();
+		
+        //Declare and initialize local variables
+        double startingUsDollars = 6500.00;		// starting US Dollars
+
+        double pesosSpent = 7210.25;            								// Mexican Pesos spent
+        double pesoExchangeRate = 18.78218;     								// 1 US dollar = 18.78218 Pesos
+        double dollarsSpentInMexico = pesosSpent / pesoExchangeRate;      		// US dollars spent in Mexico
+        double dollarsAfterMexico = startingUsDollars - dollarsSpentInMexico;   // US dollars remaining after Mexico
+        //remaining variables below here
+		// Conversion
+        // code goes below here
+		
+		double eurosSpent = 550.25;												// Euros spent
+		double euroExchangeRate = 0.857456;										// 1 US dollar = 0.857456 Euros
+		double dollarsSpentInItaly = eurosSpent / euroExchangeRate;				// US dollars spent in Italy
+		double dollarsAfterItaly = dollarsAfterMexico - dollarsSpentInItaly;	// US dollars remaining after Italy
+
+		double cadSpent = 683.20;												// CAD spent
+		double cadExchangeRate = 1.377230; 										// 1 US dollar = 1.377230 CAD
+		double dollarsSpentInCanada = cadSpent / cadExchangeRate;				// US dollars spent in Canada
+		double dollarsAfterCanada = dollarsAfterItaly - dollarsSpentInCanada;	// US dollars remaining after Canada
+
+
+
+        // Complete the code below for Souvenir Purchases
+ 		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+		System.out.println("Souvenir Purchases");
+		System.out.println(" (all values in US Dollars) ");
+		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+		//Calculations for Souvenir #1
+		int costItem1 = 12;  						//cost per item of first souvenir
+		int budget1 = 100;   						//budget for first item
+		int totalItems1 = 0; 		//how many items can be purchased
+		int fundsRemaining1 = 0;  //how much of the budget is left
+
+		System.out.println("Item 1");
+		System.out.println("   Cost per item: $" + costItem1 );
+		System.out.println("   Budget: $"+ budget1);
+		System.out.println("   Total items purchased: " +  totalItems1);
+		System.out.println("   Funds remaining: $"  +  fundsRemaining1);
+     	System.out.println();
+
+		//Calculations for Souvenir #2
+		double costItem2 = 29.99;  						//cost per item of second souvenir
+		int budget2 = 500;   							//budget for second item
+		int totalItems2 = 0; 	//how many items can be purchased
+		double fundsRemaining2 = 0.0;  	//how much of the budget is left
+
+		System.out.println("Item 2");
+		System.out.println("   Cost per item: $" + costItem2 );
+		System.out.println("   Budget: $"+ budget2);
+		System.out.println("   Total items purchased: " +  totalItems2);
+		System.out.println("   Funds remaining: $"  +  fundsRemaining2);
+
+    } // end of main method
+} // end of class
+

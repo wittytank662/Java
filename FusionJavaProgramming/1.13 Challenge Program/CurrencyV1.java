4302310da80595350e13f845e559b2e57053ac61
Sam <78057250+wittytank662@users.noreply.github.com>
2025-09-08 15:30:25 -0700

    completed challenge program

--- FusionJavaProgramming/1.13 Challenge Program/CurrencyV1.java
+++ FusionJavaProgramming/1.13 Challenge Program/CurrencyV1.java
@@ -23,10 +23,18 @@ public class CurrencyV1
         //Declare and initialize local variables
         double startingUsDollars = 6500.00;		// starting US Dollars
 
+		System.out.println("Starting US dollars: "+ startingUsDollars);
+		
         double pesosSpent = 7210.25;            								// Mexican Pesos spent
         double pesoExchangeRate = 18.78218;     								// 1 US dollar = 18.78218 Pesos
         double dollarsSpentInMexico = pesosSpent / pesoExchangeRate;      		// US dollars spent in Mexico
         double dollarsAfterMexico = startingUsDollars - dollarsSpentInMexico;   // US dollars remaining after Mexico
+
+		System.out.println("Mexico:");
+		System.out.println("	Pesos spent: " + pesosSpent);
+		System.out.println("	US dollars equivalent: " + dollarsSpentInMexico);
+		System.out.println("	US dollars remaining: " + dollarsAfterMexico);
+		
         //remaining variables below here
 		// Conversion
         // code goes below here
@@ -36,10 +44,23 @@ public class CurrencyV1
 		double dollarsSpentInItaly = eurosSpent / euroExchangeRate;				// US dollars spent in Italy
 		double dollarsAfterItaly = dollarsAfterMexico - dollarsSpentInItaly;	// US dollars remaining after Italy
 
+		System.out.println("Italy:");
+		System.out.println("	Euros spent: " + eurosSpent);
+		System.out.println("	US dollars equivalent: " + dollarsSpentInItaly);
+		System.out.println("	US dollars remaining: " + dollarsAfterItaly);
+
 		double cadSpent = 683.20;												// CAD spent
 		double cadExchangeRate = 1.377230; 										// 1 US dollar = 1.377230 CAD
 		double dollarsSpentInCanada = cadSpent / cadExchangeRate;				// US dollars spent in Canada
 		double dollarsAfterCanada = dollarsAfterItaly - dollarsSpentInCanada;	// US dollars remaining after Canada
+
+		System.out.println("Canada:");
+		System.out.println("	Canadian dollars spent: " + cadSpent);
+		System.out.println("	US dollars equivalent: " + dollarsSpentInCanada);
+		System.out.println("	US dollars remaining: " + dollarsAfterCanada);
+		System.out.println("====================================================");
+		System.out.println("Remaining US dollars: "+ dollarsAfterCanada);
+
 
 
 
@@ -52,8 +73,8 @@ public class CurrencyV1
 		//Calculations for Souvenir #1
 		int costItem1 = 12;  						//cost per item of first souvenir
 		int budget1 = 100;   						//budget for first item
-		int totalItems1 = 0; 		//how many items can be purchased
-		int fundsRemaining1 = 0;  //how much of the budget is left
+		int totalItems1 = 8; 		//how many items can be purchased
+		int fundsRemaining1 = (budget1 - (costItem1 * totalItems1));  //how much of the budget is left
 
 		System.out.println("Item 1");
 		System.out.println("   Cost per item: $" + costItem1 );
@@ -65,8 +86,8 @@ public class CurrencyV1
 		//Calculations for Souvenir #2
 		double costItem2 = 29.99;  						//cost per item of second souvenir
 		int budget2 = 500;   							//budget for second item
-		int totalItems2 = 0; 	//how many items can be purchased
-		double fundsRemaining2 = 0.0;  	//how much of the budget is left
+		int totalItems2 = 16; 	//how many items can be purchased
+		double fundsRemaining2 = (budget2 - (costItem2 * totalItems2));  	//how much of the budget is left
 
 		System.out.println("Item 2");
 		System.out.println("   Cost per item: $" + costItem2 );

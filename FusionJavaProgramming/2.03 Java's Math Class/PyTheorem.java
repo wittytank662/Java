d43d2ba1bd6a941eaecbeda182d7c724ad31d847
Sam <78057250+wittytank662@users.noreply.github.com>
2025-09-10 14:27:39 -0700

    completed 2.03

---
+++ FusionJavaProgramming/2.03 Java's Math Class/PyTheorem.java
@@ -0,0 +1,32 @@
+
+public class PyTheorem {
+
+    public static void main(String[] args) {
+        
+        // Get all the variables
+
+        // Triangle 1
+        int a = (int)(Math.random() * (23 - 5 + 1)) + 5;
+        int b = (int)(Math.random() * (23 - 5 + 1)) + 5;
+        double c = (Math.sqrt((a * a) + (b * b)));
+
+        // Triangle 2
+        int d = (int)(Math.random() * (23 - 5 + 1)) + 5;
+        int e = (int)(Math.random() * (23 - 5 + 1)) + 5;
+        double f = (Math.sqrt((d * d) + (e * e)));
+
+        // Triangle 1
+        System.out.print("Triangle 1   ");
+        System.out.print("Side 1: "+ a + "    ");
+        System.out.print("Side 2: "+ b + "    ");
+        System.out.print("Hypotenuse: "+ c);
+        System.out.println();
+
+        // Triangle 2
+        System.out.print("Triangle 2   ");
+        System.out.print("Side 1: "+ d + "    ");
+        System.out.print("Side 2: "+ e + "    ");
+        System.out.print("Hypotenuse: "+ f);
+        System.out.println();
+    }
+}

06ab5cb0b95104cbee033e677ebe48fbfd81e2d6
Sam <78057250+wittytank662@users.noreply.github.com>
2025-10-06 15:37:06 -0700

    completed some stuff

---
+++ FusionJavaProgramming/Module 3 Conditional Statements/Conditianals.java
@@ -0,0 +1,35 @@
+public class Conditianals{
+    public static void main(String[] args){
+        /*
+            Boolean Expressions
+
+                operations of comparison
+
+                    < - Less than
+                    > - Greater than
+                    <= - Less than or equal to
+                    >= - Greater than or equal to
+                    == - Equal to
+                    != - Not equal to 
+
+                    *Primitive data types
+
+            Conditional Statements
+
+                if-else:
+
+                    Structure:
+                        if(<boolean expression>){
+
+                        }
+                        else if (<boolean expression>){
+
+                        }
+                        ...
+                        else{
+                            
+                        }
+
+        */
+    }
+}

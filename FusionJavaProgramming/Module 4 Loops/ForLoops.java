c820bdb29dce1cfb56b309a5dbb45b7d26f52858
Sam <78057250+wittytank662@users.noreply.github.com>
2025-10-27 23:54:31 -0700

    hello

--- FusionJavaProgramming/Module 4 Loops/ForLoops.java
+++ FusionJavaProgramming/Module 4 Loops/ForLoops.java
@@ -36,14 +36,6 @@ public class ForLoops {
                         }
 
          */
-
-        for(int outer = 0; outer < 5; outer += 2)
-        {
-            for(int inner = 0; inner < outer + 1; inner++)
-            {
-                System.out.print("*");
-            }
-        System.out.println();
 }
          
     }

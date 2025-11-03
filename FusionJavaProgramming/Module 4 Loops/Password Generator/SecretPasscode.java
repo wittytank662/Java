019d2c0681b4c7ac8156e874a7cfe622d8abef36
Sam <78057250+wittytank662@users.noreply.github.com>
2025-11-03 15:43:41 -0800

    i did it@@

--- FusionJavaProgramming/Module 4 Loops/Password Generator/SecretPasscode.java
+++ FusionJavaProgramming/Module 4 Loops/Password Generator/SecretPasscode.java
@@ -8,13 +8,18 @@ import java.util.Scanner;
 public class SecretPasscode {
     public static void main(String[] args) throws IOException {
     
+        // Intro text
+        System.out.println("**********************************************************");
+        System.out.println("Welcome to a simple password generator.");
+        System.out.println("The password will include lowercase, uppercase, & numbers.");
+
         // Initializing
         String password = "";
         String temp = "";
         String len = "temp";
 
         int passwordMinLen = 6;
-        double passwordLen = 0;
+        int passwordLen = 0;
 
         Random passwordChar = new Random();
         Random charLimit = new Random();
@@ -30,17 +35,35 @@ public class SecretPasscode {
             System.out.println("Enter a password length (6 or more)");
             len = input.nextLine();
             passwordLen = Integer.parseInt(len);
+
+            if (passwordLen < passwordMinLen) {
+                System.out.println("    Password length too short. Please try again.");
+            }
         }
 
         // Create password
         for(int i = 0 ; i != passwordLen ; i++) {
 
-            char tempChar = (char) passwordChar.nextInt(48, 123);
+            int rangeChoice = charLimit.nextInt(2);
+
+            int randNum;
+            if (rangeChoice == 0) {
+                randNum = passwordChar.nextInt(90 - 46 + 1) + 48;
+            } else {
+                randNum = passwordChar.nextInt(122 - 97 + 1) + 97;
+            }
+
+            char tempChar = (char)randNum;
 
             temp = Character.toString(tempChar);
             password = temp + password;
         }
         outFile.println(password);
         outFile.close();
+
+        System.out.println("Generated password: " + password);
+        System.out.println("password saved to passwords.txt");
+
+        input.close();
     }
 }

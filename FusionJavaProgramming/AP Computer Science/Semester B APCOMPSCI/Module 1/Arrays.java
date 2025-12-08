53aa65a5dfb12ed644ed5b3ae981a8f73b37de96
Sam <78057250+wittytank662@users.noreply.github.com>
2025-12-08 15:52:07 -0800

    uhh update!

---
+++ FusionJavaProgramming/AP Computer Science/Semester B APCOMPSCI/Module 1/Arrays.java
@@ -0,0 +1,9 @@
+public class Arrays {
+    public static void main(String[] args) {
+        /*
+            Arrays:
+                *Arrays are collections of a given data type.
+                *Arrays are static data structures and therefore can not change size.
+        */
+    }    
+}

5a7cd6b9d0e6424b451fa05df77aac70252e453f
Sam <78057250+wittytank662@users.noreply.github.com>
2025-12-08 16:21:15 -0800

    completed part 1 of assignment

--- FusionJavaProgramming/AP Computer Science/Semester B APCOMPSCI/Module 1/Arrays.java
+++ FusionJavaProgramming/AP Computer Science/Semester B APCOMPSCI/Module 1/Arrays.java
@@ -4,6 +4,33 @@ public class Arrays {
             Arrays:
                 *Arrays are collections of a given data type.
                 *Arrays are static data structures and therefore can not change size.
+
+                Declaring and Initializing:
+                    1. Empty array of a given size:
+                    
+                        int[] arr = new int[int size];
+                    
+                    2. Explicit array with values:
+
+                        int[] arr = {10, 20, 30, 40, 50};
+
+                Accessing Arrays:
+                    We use indices which begin at 0.
+                    Negative indices do not work in Java.
+
+                    1. Store a value from an array into a variable:
+                        char c = charArr[index];
+
+                    2. Change a value of an element from an array:
+                        str Arr[index] = "Banana";
+                    
+
+                Useful Tools:
+                    Length of array: 
+                        arr.length;
+
+                    Cloning an array:
+                        arr.clone()
         */
     }    
 }

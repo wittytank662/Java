ff281f1ce9107b5e4b8ec63ba10ec4c34a4adfaf
Sam <78057250+wittytank662@users.noreply.github.com>
2026-03-04 10:07:44 -0800

    complete

--- FusionJavaProgramming/APComputerScience/SemesterBAPCOMPSCI/Module2/2.05Assignment/Catapult.java
+++ FusionJavaProgramming/APComputerScience/SemesterBAPCOMPSCI/Module2/2.05Assignment/Catapult.java
@@ -2,8 +2,16 @@ public class Catapult {
     private int speed;
     private int angle;
 
-    public Catapult(int s, int a) {
-        speed = s;
-        angle = a;
+    public Catapult() {
+        speed = 0;
+        angle = 0;
+    }
+
+    public double calcDistance(int speed,int angle) {
+        double speedFtPerSec = speed * 1.46667;
+        double gravity = 32.174;
+        double radians = Math.toRadians(angle);
+        double distance = (speedFtPerSec * speedFtPerSec) * Math.sin(2 * radians) / gravity;
+        return distance;
     }
 }
--- FusionJavaProgramming/APComputerScience/SemesterBAPCOMPSCI/Module2/2.05Assignment/CatapultTester.java
+++ FusionJavaProgramming/APComputerScience/SemesterBAPCOMPSCI/Module2/2.05Assignment/CatapultTester.java
@@ -1,14 +1,37 @@
 public class CatapultTester {
+
+    public static void printResults(double[][] array, int[] angleArray) {
+        System.out.printf("%n%40s%n", "Projectile Distance (feet)");
+        System.out.printf("%-6s", "MPH");
+        for (int angle: angleArray) {
+            System.out.printf("%9s", angle + " deg");
+        }
+        System.out.println();
+        System.out.println("=".repeat(6 + angleArray.length * 9));
+
+        for (double[] row : array) {
+            System.out.printf("%-6.0f", row[0]);
+            for (int col = 1; col < row.length; col++) {
+                System.out.printf("%9.1f", row[col]);
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
-        int[][] dist = {
-        //MPH/Deg/Deg/Deg/Deg/Deg/Deg
-            {20, 0, 0, 0, 0, 0, 0},
-            {25, 0, 0, 0, 0, 0, 0},
-            {30, 0, 0, 0, 0, 0, 0},
-            {35, 0, 0, 0, 0, 0, 0},
-            {40, 0, 0, 0, 0, 0, 0},
-            {45, 0, 0, 0, 0, 0, 0},
-            {50, 0, 0, 0, 0, 0, 0},
-        };
+        int[] speedArray = {20, 25, 30, 35, 40, 45, 50};
+        int[] angleArray = {25, 30, 35, 40, 45, 50};
+
+        double[][] twoDimArray = new double[speedArray.length][angleArray.length + 1];
+
+        Catapult myCatapult = new Catapult();
+
+        for (int i = 0; i < speedArray.length; i++) {
+            twoDimArray[i][0] = speedArray[i];
+            for (int j = 0; j < angleArray.length; j++) {
+                twoDimArray[i][j + 1] = myCatapult.calcDistance(speedArray[i], angleArray[j]);
+            }
+        }
+
+        printResults(twoDimArray, angleArray);
     }
 }
